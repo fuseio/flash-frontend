41ca1c0a19a59e6b94b550b39f9bd69e9b9344b8
Samet Sahin <a.sahin.ual@gmail.com>
2025-05-30 23:41:19 +0300

    add action buttons into card details page

--- app/(protected)/(tabs)/card/details.tsx
+++ app/(protected)/(tabs)/card/details.tsx
@@ -1,7 +1,7 @@
 import { Image } from "expo-image";
+import { Plus } from "lucide-react-native";
 import React from "react";
 import { View } from "react-native";
-import { Plus } from "lucide-react-native";
 
 import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
@@ -17,7 +17,7 @@ export default function CardDetails() {
   return (
     <View className="flex-1 bg-background p-6">
       {/* Balance Information */}
-      <View className="items-center mb-6">
+      <View className="items-center mt-10 mb-6">
         <View className="flex-row items-baseline">
           <Text className="text-xl font-semibold">$</Text>
           <Text className="text-[50px] font-semibold">{cardData.balance.substring(1)}</Text>
@@ -26,7 +26,7 @@ export default function CardDetails() {
       </View>
 
       <Button
-        className="rounded-3xl h-14 w-auto self-center mb-6"
+        className="rounded-3xl h-10 w-auto self-center mb-6"
       >
         <View className="flex-row">
           <Plus color="black" />
@@ -38,9 +38,42 @@ export default function CardDetails() {
         <Image
           source={require("@/assets/images/card.png")}
           alt="Flash Card"
-          style={{ width: '100%', height: '80%' }}
+          style={{ width: 800, height: 600 }}
           contentFit="contain"
         />
+      </View>
+
+      {/* Circular Action Buttons */}
+      <View className="flex-row justify-around items-center w-1/4 self-center">
+        <View className="items-center">
+          <Button className="w-16 h-16 rounded-full bg-gray-100 border border-gray-200">
+            <Image
+              source={require("@/assets/images/settings.png")}
+              style={{ width: 34, height: 34 }}
+            />
+          </Button>
+          <Text className="text-xs mt-2 text-[#BFBFBF]">Settings</Text>
+        </View>
+        
+        <View className="items-center">
+          <Button className="w-16 h-16 rounded-full bg-gray-100 border border-gray-200">
+            <Image
+              source={require("@/assets/images/limit.png")}
+              style={{ width: 34, height: 34 }}
+            />
+          </Button>
+          <Text className="text-xs mt-2 text-[#BFBFBF]">Limit</Text>
+        </View>
+        
+        <View className="items-center">
+          <Button className="w-16 h-16 rounded-full bg-gray-100 border border-gray-200">
+            <Image
+              source={require("@/assets/images/freeze.png")}
+              style={{ width: 28, height: 28 }}
+            />
+          </Button>
+          <Text className="text-xs mt-2 text-[#BFBFBF]">Freeze</Text>
+        </View>
       </View>
     </View>
   );
