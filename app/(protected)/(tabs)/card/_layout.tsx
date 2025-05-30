9b5d32f9c986d152d9b1c46456c81ce42c5782ea
Samet Sahin <a.sahin.ual@gmail.com>
2025-05-30 23:44:19 +0300

    remove the stack of card details page from the layout since it's not necessary

--- app/(protected)/(tabs)/card/_layout.tsx
+++ app/(protected)/(tabs)/card/_layout.tsx
@@ -4,25 +4,7 @@ export default function CardLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen 
-        name="details" 
-        options={{ 
-          presentation: 'modal',
-          headerShown: true,
-          headerTitle: "Card",
-          headerTitleAlign: "center",
-          headerStyle: {
-            backgroundColor: "#262626",
-          },
-          headerTitleStyle: {
-            color: "#ffffff",
-            fontFamily: "System",
-            fontWeight: "bold",
-            fontSize: 20
-          },
-          headerTintColor: "#ffffff",
-        }} 
-      />
+      
     </Stack>
   );
 } 
