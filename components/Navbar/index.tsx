60d22a6edaa128f3172de509ac21a1bcaa7d36ed
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-06-09 20:52:15 +0500

    Update Welcome page v2

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import { Image } from "expo-image"
 import { Link } from "expo-router"
-import { View } from "react-native"
+import { SafeAreaView, View } from "react-native"
 
 import AccountCenterModal from "../AccountCenter/AccountCenterModal";
 import { path } from "@/constants/path";
@@ -8,27 +8,29 @@ import { NavMenu } from "./NavMenu";
 
 const Navbar = () => {
   return (
-    <View className="bg-background flex-row justify-end md:justify-between items-center p-4 md:p-6">
-      <Link href={path.HOME} className="hidden md:flex flex-row items-center gap-2">
-        <Image
-          source={require("@/assets/images/solid-logo.png")}
-          alt="Solid logo"
-          contentFit="contain"
-          style={{ width: 32, height: 32 }}
-        />
-        <Image
-          source={require("@/assets/images/solid.png")}
-          alt="Solid"
-          contentFit="contain"
-          style={{ width: 72, height: 37 }}
-          className="hidden md:block"
-        />
-      </Link>
-      <NavMenu />
-      <View className="w-32">
-        <AccountCenterModal />
+    <SafeAreaView>
+      <View className="bg-background flex-row justify-end md:justify-between items-center p-4 md:p-6">
+        <Link href={path.HOME} className="hidden md:flex flex-row items-center gap-2">
+          <Image
+            source={require("@/assets/images/solid-logo.png")}
+            alt="Solid logo"
+            contentFit="contain"
+            style={{ width: 32, height: 32 }}
+          />
+          <Image
+            source={require("@/assets/images/solid.png")}
+            alt="Solid"
+            contentFit="contain"
+            style={{ width: 72, height: 37 }}
+            className="hidden md:block"
+          />
+        </Link>
+        <NavMenu />
+        <View className="w-32">
+          <AccountCenterModal />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
