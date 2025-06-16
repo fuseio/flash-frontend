e89ee326fbe8e8f4ced2c617bfbc92cc380883a8
Samet Sahin <a.sahin.ual@gmail.com>
2025-06-16 19:55:09 +0300

    show an AppBar in card details screen

--- app/(protected)/(tabs)/card/_layout.tsx
+++ app/(protected)/(tabs)/card/_layout.tsx
@@ -1,9 +1,40 @@
-import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { Stack, router } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
 
 export default function CardLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#262626',
+        },
+        headerTitleAlign: 'center',
+        headerTintColor: '#ffffff',
+        headerTitleStyle: {
+          color: '#ffffff',
+          fontSize: 20,
+          fontWeight: 'bold',
+        },
+        headerLeft: ({ canGoBack, tintColor }) => 
+          canGoBack ? (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons 
+                name="chevron-back" 
+                size={24} 
+                color={tintColor} 
+              />
+            </TouchableOpacity>
+          ) : null,
+      }}
+    >
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen 
+        name="details_mobile" 
+        options={{ 
+          title: "Solid card",
+        }} 
+      />
     </Stack>
   );
 } 
